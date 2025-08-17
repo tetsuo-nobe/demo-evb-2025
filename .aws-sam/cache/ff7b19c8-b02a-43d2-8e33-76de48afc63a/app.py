a961cb338434ea10ae647d9be93b44508bd8302f
Workshop Participant <participant@workshops.aws>
2025-08-17 23:36:10 +0000

    first commit

---
+++ .aws-sam/cache/ff7b19c8-b02a-43d2-8e33-76de48afc63a/app.py
@@ -0,0 +1,32 @@
+import json
+import boto3
+import datetime
+import base64
+from aws_xray_sdk.core import patch
+patch(['boto3'])
+
+def lambda_handler(event, context):
+    #
+    #order = event["order"]
+    order = { "orderId": "Mod9Demo","orderItem": "Donuts","quantity": "1", "receiptRequired": "true"}
+    client = boto3.client('events')
+    # 
+    client.put_events(
+        Entries=[{
+            "Time": datetime.datetime.now(),
+            "Source": "demo.eventbridge.order",
+            "DetailType": "OrderCreated",
+            "Resources": [
+               order["orderId"]
+            ],
+            "Detail": json.dumps(order),
+            "EventBusName": "EventBridgeDemo-bus"
+        }]
+    )
+    #
+    return {
+        'statusCode': 200,
+        'body': json.dumps('** The order event was issued **')
+    }
+
+
